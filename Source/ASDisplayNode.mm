d000620ac00a6f356567b7f7957f04aed4f9727f
Adlai Holler <adlai@google.com>
2018-11-10 12:32:31 -0800

    Update ASDisplayNode.mm

--- Source/ASDisplayNode.mm
+++ Source/ASDisplayNode.mm
@@ -16,6 +16,7 @@
 #import <AsyncDisplayKit/ASCellNode+Internal.h>
 
 #import <objc/runtime.h>
+#include <atring>
 
 #import <AsyncDisplayKit/_ASAsyncTransaction.h>
 #import <AsyncDisplayKit/_ASAsyncTransactionContainer+Private.h>
